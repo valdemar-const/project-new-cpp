d80cf88163987feb2d925bdcd1d41bbe2b990af7
Vladimir Novikov <vladimir.novikov.k@yandex.ru>
2024-09-10 01:24:34 +0300

    Nice meta generated context

--- projects/lib/glmeta/include/glmeta/glmeta.hpp
+++ projects/lib/glmeta/include/glmeta/glmeta.hpp
@@ -10,18 +10,18 @@
 #include <type_traits>
 #include <compare>
 
-#include <exception>
+#include <stdexcept>
 
 namespace glmeta
 {
 
-namespace basic_types
+namespace types
 {
     using GLenum  = unsigned int;
     using GLubyte = khronos_uint8_t;
     using GLint   = int;
     using GLuint  = unsigned int;
-} // namespace basic_types
+} // namespace types
 
 namespace constants
 {
@@ -30,7 +30,7 @@ namespace constants
 
 namespace functions
 {
-    using namespace basic_types;
+    using namespace types;
 
     using GL_Any_Proc  = void();
     using GL_Load_Proc = std::function<GL_Any_Proc *(std::string_view)>;
@@ -38,9 +38,60 @@ namespace functions
     template<typename Result, typename... Args>
     using GL_Proc = Result(Args...);
 
-    using glGetString   = GL_Proc<const GLubyte *, GLenum>;
-    using glGetIntegerv = GL_Proc<void, GLenum, GLint *>;
-    using glGetStringi  = GL_Proc<const GLubyte *, GLenum, GLuint>;
+    template<typename Result, typename... Args>
+    class GL_Functor
+    {
+      public:
+
+        using Proc_Type = GL_Proc<Result, Args...>;
+        const std::string func_name;
+
+      public:
+
+        GL_Functor(std::string_view func_name, GL_Load_Proc gl_load_proc, bool is_lazy = false)
+            : func_name(func_name)
+            , gl_load_proc_(gl_load_proc)
+            , proc_(reinterpret_cast<Proc_Type *>((is_lazy) ? nullptr : gl_load_proc(func_name)))
+            , is_loaded_(!is_lazy)
+        {
+        }
+
+        bool
+        empty() const
+        {
+            return is_loaded_ && proc_ == nullptr;
+        }
+
+        Result
+        operator()(Args... args)
+        {
+            if (proc_)
+            {
+                return proc_(args...);
+            }
+            else if (!is_loaded_)
+            {
+                proc_      = reinterpret_cast<Proc_Type *>(gl_load_proc_(func_name));
+                is_loaded_ = true;
+                return proc_(args...);
+            }
+            else
+            {
+                std::string error_msg {"OpenGL function " + func_name + " not loaded from context"};
+                throw std::runtime_error(error_msg);
+            }
+        }
+
+      private:
+
+        GL_Load_Proc gl_load_proc_;
+        Proc_Type   *proc_      = nullptr;
+        bool         is_loaded_ = false;
+    };
+
+    using glGetString   = GL_Functor<const GLubyte *, GLenum>;
+    using glGetIntegerv = GL_Functor<void, GLenum, GLint *>;
+    using glGetStringi  = GL_Functor<const GLubyte *, GLenum, GLuint>;
 
 } // namespace functions
 
@@ -57,7 +108,8 @@ namespace exceptions
 enum class Client_Api : uint8_t
 {
     Gl,
-    Gles
+    Gles1,
+    Gles2
 };
 
 enum class Profile : uint8_t
@@ -76,8 +128,24 @@ enum class Load_Strategy : uint8_t
 namespace impl
 {
 
+    template<Client_Api Client_Api, size_t Version_Major, size_t Version_Minor, Profile Profile, typename Enable = void>
+    struct glGetStringi_enabled
+    {
+    };
+
+    template<Client_Api Client_Api, size_t Version_Major, size_t Version_Minor, Profile Profile>
+    struct glGetStringi_enabled<
+            Client_Api,
+            Version_Major,
+            Version_Minor,
+            Profile,
+            typename std::enable_if<(Client_Api == Client_Api::Gl || Client_Api == Client_Api::Gles2) && Version_Major >= 3>::type>
+    {
+        functions::glGetStringi::Proc_Type GetStringi;
+    };
+
     template<Client_Api Client_Api, size_t Version_Major, size_t Version_Minor, Profile Profile, Load_Strategy Load_Strategy, typename... Extensions>
-    class GL_Load_Context_Basic
+    class GL_Load_Context_Basic : public glGetStringi_enabled<Client_Api, Version_Major, Version_Minor, Profile>
     {
       public: // traits
       public:
@@ -85,11 +153,14 @@ namespace impl
         GL_Load_Context_Basic(functions::GL_Load_Proc gl_load_proc)
             : gl_load_proc_(gl_load_proc)
         {
+            bool is_lazy = Load_Strategy == Load_Strategy::Lazy;
+
+            GetString   = functions::glGetString {"glGetString", gl_load_proc, is_lazy};
+            GetIntegerv = functions::glGetIntegerv {"glGetIntegerv", gl_load_proc, is_lazy};
         }
 
-        functions::glGetString   GetString;
-        functions::glGetIntegerv GetIntegerv;
-        functions::glGetStringi  GetStringi;
+        std::function<functions::glGetString::Proc_Type>   GetString;
+        std::function<functions::glGetIntegerv::Proc_Type> GetIntegerv;
 
       protected:
 
--- projects/lib/glmeta/tests/test.cpp
+++ projects/lib/glmeta/tests/test.cpp
@@ -1,5 +1,7 @@
 #include <glmeta/glmeta.hpp>
 
+#include <iostream>
+
 using namespace glmeta;
 
 int
@@ -9,8 +11,9 @@ main(int argc, const char *argv[])
     {
         return reinterpret_cast<functions::GL_Any_Proc *>(nullptr);
     };
-    GL_Load_Context<Client_Api::Gl, 1, 0, Profile::None, Load_Strategy::Immediate> {gl_load_proc};
-    GL_Load_Context<Client_Api::Gl, 3, 3, Profile::Core, Load_Strategy::Immediate> {gl_load_proc};
-    GL_Load_Context<Client_Api::Gles, 3, 0, Profile::Core, Load_Strategy::Immediate> {gl_load_proc};
+    GL_Load_Context<Client_Api::Gl, 1, 0, Profile::None, Load_Strategy::Immediate>    old_gl {gl_load_proc};
+    GL_Load_Context<Client_Api::Gl, 3, 3, Profile::Core, Load_Strategy::Immediate>    new_gl {gl_load_proc};
+    GL_Load_Context<Client_Api::Gles2, 3, 0, Profile::Core, Load_Strategy::Immediate> new_gles {gl_load_proc};
+
     return 0;
 }
